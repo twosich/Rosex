20d751f04b1214a6f142294b79d3a1d096970a2b
twosich <latriasouls@protonmail.com>
2024-10-08 02:25:45 -0300

    add things

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Navbar from './lib/navbar.svelte';
+import navbar from './lib/navbar.svelte';
+</script>
+
+<div>
+  <div>
+    <Navbar />
+  </div>
+</div>
+
---
+++ src/lib/navbar.svelte
@@ -0,0 +1,8 @@
+<script>
+    
+</script>
+
+
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
