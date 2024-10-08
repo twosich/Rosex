bf287a302cfb15a3d70bbc8e0b0065d6573052c6
twosich <latriasouls@protonmail.com>
2024-10-08 04:17:23 -0300

    update responsive

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,7 @@
   </div>
 
   <div class="container mx-auto p-2">   
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+    <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-4">
       <Rules />
       <Post />
       <Post />
